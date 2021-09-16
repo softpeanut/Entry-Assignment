e8a1c4a1322d720e10d60f08598ffa3dafb77ea4
김범진 <sdpthf@naver.com>
2021-09-17 01:44:45 +0900

    [ADD] Login and Reissue Controller

--- assignment/src/main/java/com/example/assignment/controller/member/MemberController.java
+++ assignment/src/main/java/com/example/assignment/controller/member/MemberController.java
@@ -1,6 +1,8 @@
 package com.example.assignment.controller.member;
 
+import com.example.assignment.payload.member.request.LoginRequest;
 import com.example.assignment.payload.member.request.SignupRequest;
+import com.example.assignment.payload.member.response.TokenResponse;
 import com.example.assignment.service.member.MemberServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +19,16 @@ public class MemberController {
     @ResponseStatus(HttpStatus.CREATED)
     public void signup(@Valid @RequestBody SignupRequest request) {
         memberService.signup(request);
+    }
+
+    @PostMapping("/auth/login")
+    public TokenResponse login(@Valid @RequestBody LoginRequest request) {
+        return memberService.login(request);
+    }
+
+    @PutMapping("/auth/reissue")
+    public TokenResponse reissue(@RequestHeader(name = "X-Refresh-Token") String token) {
+        return memberService.reissue(token);
     }
 
 }
