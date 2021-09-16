cebcbeabbc3a90fa16cc9c58dd0713b852b1b701
김범진 <sdpthf@naver.com>
2021-09-16 16:57:34 +0900

    [ADD] MemberRepository

---
+++ assignment/src/main/java/com/example/assignment/entity/member/MemberRepository.java
@@ -0,0 +1,6 @@
+package com.example.assignment.entity.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Integer> {
+}
