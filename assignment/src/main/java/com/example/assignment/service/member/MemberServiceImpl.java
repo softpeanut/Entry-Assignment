64d0c910d47398f16647738994e2b88666918e94
김범진 <sdpthf@naver.com>
2021-09-17 01:42:16 +0900

    [UPDATE] MemberServiceImpl

--- assignment/src/main/java/com/example/assignment/service/member/MemberServiceImpl.java
+++ assignment/src/main/java/com/example/assignment/service/member/MemberServiceImpl.java
@@ -3,20 +3,29 @@ package com.example.assignment.service.member;
 import com.example.assignment.entity.member.Member;
 import com.example.assignment.entity.member.MemberRepository;
 import com.example.assignment.entity.member.Role;
-import com.example.assignment.exception.MemberNameAlreadyExistsException;
-import com.example.assignment.exception.MemberUsernameAlreadyExistsException;
+import com.example.assignment.entity.refreshtoken.RefreshToken;
+import com.example.assignment.entity.refreshtoken.RefreshTokenRepository;
+import com.example.assignment.exception.*;
 import com.example.assignment.payload.member.request.LoginRequest;
 import com.example.assignment.payload.member.request.SignupRequest;
 import com.example.assignment.payload.member.response.TokenResponse;
+import com.example.assignment.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
+
+    @Value("${jwt.exp.refresh}")
+    private Long refreshTokenExpirationTime;
+
     private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtTokenProvider tokenProvider;
 
     @Override
     public void signup(SignupRequest request) {
@@ -39,12 +48,41 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public TokenResponse login(LoginRequest request) {
+        Member member = memberRepository.findByUsername(request.getUsername())
+                .orElseThrow(MemberNotFoundException::new);
+
+        if(!passwordEncoder.matches(request.getPassword(), member.getPassword()))
+            throw new InvalidPasswordException();
+
+        return createToken(request.getUsername());
 
     }
 
     @Override
-    public TokenResponse reissue(String token) {
+    public TokenResponse reissue(String refreshToken) {
+        if(!tokenProvider.isRefreshToken(refreshToken) || !tokenProvider.validateToken(refreshToken))
+            throw new InvalidTokenException();
 
+        RefreshToken newRefreshToken = refreshTokenRepository.findByRefreshToken(refreshToken)
+                .map(token -> token.updateExpiration(refreshTokenExpirationTime))
+                .orElseThrow();
+
+        return new TokenResponse(tokenProvider.createAccessToken(newRefreshToken.getUsername()), refreshToken);
+    }
+
+    public TokenResponse createToken(String username) {
+        String accessToken = tokenProvider.createAccessToken(username);
+        String refreshToken = tokenProvider.createRefreshToken(username);
+
+        refreshTokenRepository.save(
+                RefreshToken.builder()
+                        .username(username)
+                        .refreshToken(refreshToken)
+                        .expiration(refreshTokenExpirationTime)
+                        .build()
+        );
+
+        return new TokenResponse(accessToken, refreshToken);
     }
 
 }
