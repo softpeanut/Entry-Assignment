8bc2cf5f86785efaa13d4e80c969e45c2c1f741d
김범진 <sdpthf@naver.com>
2021-09-16 23:35:37 +0900

    [UPDATE] CSRF Config Disable

--- assignment/src/main/java/com/example/assignment/security/config/SecurityConfig.java
+++ assignment/src/main/java/com/example/assignment/security/config/SecurityConfig.java
@@ -26,8 +26,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .and().formLogin().disable()
+                .csrf().disable()
+                .formLogin().disable()
 
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
