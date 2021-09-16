65193baddbb37cdba0a6aa36b517c69dd1d56a14
김범진 <sdpthf@naver.com>
2021-09-16 16:56:42 +0900

    [ADD] BookMark Entity

---
+++ assignment/src/main/java/com/example/assignment/entity/bookmark/BookMark.java
@@ -0,0 +1,29 @@
+package com.example.assignment.entity.bookmark;
+
+import com.example.assignment.entity.feed.Feed;
+import com.example.assignment.entity.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class BookMark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Feed feed;
+}
