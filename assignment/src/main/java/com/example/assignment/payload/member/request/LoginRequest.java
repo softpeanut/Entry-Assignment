6e052897f424c35b3850434f556fafb95c31c1fd
김범진 <sdpthf@naver.com>
2021-09-17 01:39:46 +0900

    [UPDATE] LoginRequest Notbalnk

--- assignment/src/main/java/com/example/assignment/payload/member/request/LoginRequest.java
+++ assignment/src/main/java/com/example/assignment/payload/member/request/LoginRequest.java
@@ -3,11 +3,16 @@ package com.example.assignment.payload.member.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
-    private String name;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
     private String password;
 }
