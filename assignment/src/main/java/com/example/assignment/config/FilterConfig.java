4ea0bf6129496293de5a522b7adbc8d40319a38e
김범진 <sdpthf@naver.com>
2021-09-17 01:43:10 +0900

    [UPDATE] Register Filters

--- assignment/src/main/java/com/example/assignment/config/FilterConfig.java
+++ assignment/src/main/java/com/example/assignment/config/FilterConfig.java
@@ -1,18 +1,24 @@
 package com.example.assignment.config;
 
 import com.example.assignment.error.ExceptionHandlerFilter;
+import com.example.assignment.security.jwt.JwtTokenFilter;
+import com.example.assignment.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
     private final ExceptionHandlerFilter exceptionHandlerFilter;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public void configure(HttpSecurity builder) throws Exception {
-
+        JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
+        builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        builder.addFilterBefore(exceptionHandlerFilter, JwtTokenFilter.class);
     }
 
 }
