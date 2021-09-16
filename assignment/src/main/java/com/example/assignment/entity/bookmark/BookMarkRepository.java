e1931c3172141f0928d429ea3c91734bedc36c17
김범진 <sdpthf@naver.com>
2021-09-16 16:56:56 +0900

    [ADD] BookMarkRepository

---
+++ assignment/src/main/java/com/example/assignment/entity/bookmark/BookMarkRepository.java
@@ -0,0 +1,6 @@
+package com.example.assignment.entity.bookmark;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookMarkRepository extends JpaRepository<BookMark, Integer> {
+}
