c87e73f586d057d45e9e05369b715000e60777b2
김범진 <sdpthf@naver.com>
2021-09-16 17:18:59 +0900

    [ADD] TokenResponse

---
+++ assignment/src/main/java/com/example/assignment/payload/member/response/TokenResponse.java
@@ -0,0 +1,11 @@
+package com.example.assignment.payload.member.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+    private String accessToken;
+    private String refreshToken;
+}
