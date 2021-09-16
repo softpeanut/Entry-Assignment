4e854ad482bda10e1dbd2bf1f4a4f4be82390bc1
김범진 <sdpthf@naver.com>
2021-09-16 16:57:17 +0900

    [ADD] FeedRepository

---
+++ assignment/src/main/java/com/example/assignment/entity/feed/FeedRepository.java
@@ -0,0 +1,6 @@
+package com.example.assignment.entity.feed;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedRepository extends JpaRepository<Feed, Integer> {
+}
