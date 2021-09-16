eee3151f2ccffbbc8fa3c60c5660e3410a07bd19
김범진 <sdpthf@naver.com>
2021-09-17 01:42:43 +0900

    [ADD] RefreshTokenRepository

---
+++ assignment/src/main/java/com/example/assignment/entity/refreshtoken/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.example.assignment.entity.refreshtoken;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
