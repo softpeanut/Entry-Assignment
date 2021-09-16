9ef138c419fde306519d98df16b4a6a8bdd7fff7
김범진 <sdpthf@naver.com>
2021-09-16 23:22:41 +0900

    [UPDATE] Name and Username Repository
    Check Exists

--- assignment/src/main/java/com/example/assignment/entity/member/MemberRepository.java
+++ assignment/src/main/java/com/example/assignment/entity/member/MemberRepository.java
@@ -1,6 +1,9 @@
 package com.example.assignment.entity.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Integer> {
+    Optional<Member> findByUsername(String username);
+    Optional<Member> findByName(String name);
 }
