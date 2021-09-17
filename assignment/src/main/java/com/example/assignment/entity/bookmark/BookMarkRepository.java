499475b462f34a7441f0463c8be1d9aa6ade6c7f
김범진 <sdpthf@naver.com>
2021-09-17 11:44:49 +0900

    [UPDATE] BookMarkRepository

--- assignment/src/main/java/com/example/assignment/entity/bookmark/BookMarkRepository.java
+++ assignment/src/main/java/com/example/assignment/entity/bookmark/BookMarkRepository.java
@@ -1,6 +1,9 @@
 package com.example.assignment.entity.bookmark;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface BookMarkRepository extends JpaRepository<BookMark, Integer> {
+    Optional<BookMark> findByMemberIdAndFeedId(Integer memberId, Integer postId);
+    void deleteByMemberIdAndFeedId(Integer memberId, Integer postId);
 }
