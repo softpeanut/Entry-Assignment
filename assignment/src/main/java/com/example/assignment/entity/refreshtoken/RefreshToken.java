9b1dfe458a68a337b406661805fbf37e4f809dbd
김범진 <sdpthf@naver.com>
2021-09-18 00:06:00 +0900

    [UPDATE] RefreshToken

--- assignment/src/main/java/com/example/assignment/entity/refreshtoken/RefreshToken.java
+++ assignment/src/main/java/com/example/assignment/entity/refreshtoken/RefreshToken.java
@@ -3,7 +3,6 @@ package com.example.assignment.entity.refreshtoken;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
