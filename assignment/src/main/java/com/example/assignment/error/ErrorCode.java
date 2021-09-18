16a53037113abc4500947c8f5867d2a908582d61
김범진 <sdpthf@naver.com>
2021-09-18 17:39:30 +0900

    [UPDATE] ErrorCode

--- assignment/src/main/java/com/example/assignment/error/ErrorCode.java
+++ assignment/src/main/java/com/example/assignment/error/ErrorCode.java
@@ -7,17 +7,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    MEMBER_NAME_ALREADY_EXISTS(409, "Member Name Already Exists"),
-    MEMBER_USERNAME_ALREADY_EXISTS(409, "Member Username Already Exists"),
-    MEMBER_NOT_FOUND(404, "Member Not Found"),
-
-    INVALID_PASSWORD(401, "Invalid Password"),
     INVALID_TOKEN(401, "Invalid Token"),
+    INVALID_PASSWORD(401, "Invalid Password"),
 
     FEED_NOT_FOUND(404, "Feed Not Found"),
+    MEMBER_NOT_FOUND(404, "Member Not Found"),
+    BOOK_MARK_NOT_FOUND(404, "Bookmark Not Found"),
 
     BOOK_MARK_ALREADY_EXISTS(409, "Bookmark Already Exists"),
-    BOOK_MARK_NOT_FOUND(404, "Bookmark Not Found");
+    MEMBER_NAME_ALREADY_EXISTS(409, "Member Name Already Exists"),
+    MEMBER_USERNAME_ALREADY_EXISTS(409, "Member Username Already Exists");
 
     private int status;
     private String message;
