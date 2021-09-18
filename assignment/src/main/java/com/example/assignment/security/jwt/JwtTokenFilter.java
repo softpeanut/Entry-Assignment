9d08f8a9c79ce9d94999c0a7e6ad2497301c45cf
김범진 <sdpthf@naver.com>
2021-09-18 19:31:46 +0900

    [ADD] Component Annotation

--- assignment/src/main/java/com/example/assignment/security/jwt/JwtTokenFilter.java
+++ assignment/src/main/java/com/example/assignment/security/jwt/JwtTokenFilter.java
@@ -4,6 +4,7 @@ import com.example.assignment.exception.InvalidTokenException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class JwtTokenFilter extends OncePerRequestFilter {
     private final JwtTokenProvider tokenProvider;
