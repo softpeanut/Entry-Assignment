b879e0189cf1586fe1160643f4fe9e1bacdb4c0e
김범진 <sdpthf@naver.com>
2021-09-18 19:08:06 +0900

    [UPDATE] ExceptionResponse

--- assignment/src/main/java/com/example/assignment/error/exception/ExceptionResponse.java
+++ assignment/src/main/java/com/example/assignment/error/exception/ExceptionResponse.java
@@ -2,10 +2,8 @@ package com.example.assignment.error.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
-@ToString
 @AllArgsConstructor
 public class ExceptionResponse {
     private final String message;
