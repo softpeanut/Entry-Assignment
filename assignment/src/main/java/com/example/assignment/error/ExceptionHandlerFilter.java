c2e042189e191173e4c536b6ba0bcd6a5d2e8b24
김범진 <sdpthf@naver.com>
2021-09-19 16:13:18 +0900

    [ADD] null check

--- assignment/src/main/java/com/example/assignment/error/ExceptionHandlerFilter.java
+++ assignment/src/main/java/com/example/assignment/error/ExceptionHandlerFilter.java
@@ -4,10 +4,8 @@ import com.example.assignment.error.exception.BusinessException;
 import com.example.assignment.error.exception.ExceptionResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -33,6 +31,9 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
     }
 
     public String convertToJson(Object object) throws JsonProcessingException {
+        if(object == null)
+            return null;
+
         return new ObjectMapper().writeValueAsString(object);
     }
 
