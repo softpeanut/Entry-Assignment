e4b4334db160962b2109b60fca945e2a1592b504
김범진 <sdpthf@naver.com>
2021-09-22 11:28:39 +0900

    [UPDATE] RequestDto

--- assignment/src/main/java/com/example/assignment/payload/member/request/LoginRequest.java
+++ assignment/src/main/java/com/example/assignment/payload/member/request/LoginRequest.java
@@ -2,11 +2,9 @@ package com.example.assignment.payload.member.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotBlank;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
 
--- assignment/src/main/java/com/example/assignment/payload/member/request/UpdatePasswordRequest.java
+++ assignment/src/main/java/com/example/assignment/payload/member/request/UpdatePasswordRequest.java
@@ -2,10 +2,8 @@ package com.example.assignment.payload.member.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class UpdatePasswordRequest {
     private String password;
