2b4aedfe90c3c18115f4376652d93c8d62487513
김범진 <sdpthf@naver.com>
2021-09-25 18:41:28 +0900

    [UPDATE] ResponseDto

--- assignment/src/main/java/com/example/assignment/payload/feed/response/FeedResponse.java
+++ assignment/src/main/java/com/example/assignment/payload/feed/response/FeedResponse.java
@@ -3,11 +3,9 @@ package com.example.assignment.payload.feed.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class FeedResponse {
     private Integer id;
