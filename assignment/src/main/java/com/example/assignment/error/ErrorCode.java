013bee65d74896d72374e7cf870c567e5d47d271
김범진 <sdpthf@naver.com>
2021-09-26 18:05:56 +0900

    [UPDATE] ErrorCode

--- assignment/src/main/java/com/example/assignment/error/ErrorCode.java
+++ assignment/src/main/java/com/example/assignment/error/ErrorCode.java
@@ -7,17 +7,24 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    // 토큰
     INCORRECT_TOKEN(400, "Incorrect Token"),
     INVALID_TOKEN(401, "Invalid Token"),
-    INVALID_PASSWORD(401, "Invalid Password"),
     EXPIRED_ACCESS_TOKEN(401, "Expired Access Token"),
     EXPIRED_REFRESH_TOKEN(401, "Expired Refresh Token"),
-    FEED_NOT_FOUND(404, "Feed Not Found"),
+
+    // 회원
+    INVALID_PASSWORD(401, "Invalid Password"),
     MEMBER_NOT_FOUND(404, "Member Not Found"),
-    BOOK_MARK_NOT_FOUND(404, "Bookmark Not Found"),
-    BOOK_MARK_ALREADY_EXISTS(409, "Bookmark Already Exists"),
     MEMBER_NAME_ALREADY_EXISTS(409, "Member Name Already Exists"),
-    MEMBER_USERNAME_ALREADY_EXISTS(409, "Member Username Already Exists");
+    MEMBER_USERNAME_ALREADY_EXISTS(409, "Member Username Already Exists"),
+
+    // 게시글
+    FEED_NOT_FOUND(404, "Feed Not Found"),
+
+    // 북마크
+    BOOK_MARK_NOT_FOUND(404, "Bookmark Not Found"),
+    BOOK_MARK_ALREADY_EXISTS(409, "Bookmark Already Exists");
 
     private int status;
     private String message;
